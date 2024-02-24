c53e846fb3d5ec4b7dc5fad8b25931efb2333e51
Rory Simmonds <rory@blankbox.business>
2024-02-24 12:15:13 +0000

    update code to use ES6 import functionality

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+import Manager from '../lib/Manager'
+import Engineer from '../lib/Engineer'
+import Intern from '../lib/Intern'
+import inquirer from 'inquirer'
+import path from 'path'
+import fs from 'fs'
+
+// Replicate the functionality of __dirname for ES6 Module
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
+import render from './src/page-template.js'
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
